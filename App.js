933f661c569c8e4fcbcbffd4188cd91f023032c0
Koushik <Koushikbajpayee06@gmail.com>
2025-09-18 13:16:10 +0530

    add attribute in react element

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 
-
-        const heading = React.createElement("h1", {}, "Hello World from React!" );
-        const root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render(heading);
+const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!" )
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading);
