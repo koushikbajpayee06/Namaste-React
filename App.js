0299069c19375b69622dad55f61d49042ac5eaab
Koushik <Koushikbajpayee06@gmail.com>
2025-09-18 15:25:41 +0530

    giving two elements as a sibblings

--- App.js
+++ App.js
@@ -9,12 +9,14 @@
 const parent = React.createElement(
     "div",
     {id:"parent"},
-    React.createElement("div",{id:"child"},React.createElement("h1",{},"Hi I am h1 Tag")
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"Hi I am h1 Tag"),
+        React.createElement("h2",{},"Hi I am h2 Tag")]
     )
 );
 
 
 // const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!" )
 // console.log(heading);
-// const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(parent);
