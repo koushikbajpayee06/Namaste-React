8ea4a05321e961a07227b2fca08401c15e1724e0
Koushik <Koushikbajpayee06@gmail.com>
2025-09-18 15:31:57 +0530

    adding two child under one parent

--- App.js
+++ App.js
@@ -3,17 +3,28 @@
  *      <div id='child'>
  *          <h1></h1>
  *      </div>
+ *      <div id='child2'>
+ *          <h2></h2>
+ *      </div>
  *  </div>
- */ 
+ */
 
-const parent = React.createElement(
+ const parent = React.createElement(
     "div",
     {id:"parent"},
-    React.createElement("div",{id:"child"},
+    [React.createElement("div",{id:"child"},
         [React.createElement("h1",{},"Hi I am h1 Tag"),
         React.createElement("h2",{},"Hi I am h2 Tag")]
-    )
+    ),
+    React.createElement("div",{id:"child2"},
+        [React.createElement("h1",{},"Hi I am h1 Tag"),
+        React.createElement("h2",{},"Hi I am h2 Tag")]
+    ),
+    ]
 );
+ 
+
+
 
 
 // const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!" )
