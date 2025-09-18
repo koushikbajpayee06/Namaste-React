b850711a6f738165be010d3cf4703c02fe28981f
Koushik <Koushikbajpayee06@gmail.com>
2025-09-18 15:19:23 +0530

    added nested element structure inside react app

--- App.js
+++ App.js
@@ -1,4 +1,20 @@
+/**
+ *  <div id="parent">
+ *      <div id='child'>
+ *          <h1></h1>
+ *      </div>
+ *  </div>
+ */ 
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!" )
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading);
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement("div",{id:"child"},React.createElement("h1",{},"Hi I am h1 Tag")
+    )
+);
+
+
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!" )
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent);
