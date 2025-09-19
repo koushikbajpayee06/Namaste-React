99e20836d5350db30c46aadb081d9e6b2dcf41b3
Koushik <Koushikbajpayee06@gmail.com>
2025-09-20 00:22:52 +0530

    reaact functional Component

--- App.js
+++ App.js
@@ -4,7 +4,15 @@ import ReactDOM from "react-dom/client"
 // React.createElement ==> Object
 
 const heading = <h1 id="heading">Namaste React Using JSX</h1>
+console.log(heading);
 
 // const heading = React.createElement("h1",{id:"heading"},"Namaste Heading");
+//React Component
+const HeadingComponent= ()=>{
+    return <h1>Namaste react</h1>
+}
+
+const HeadingComponent2 = ()=> (<h1>Namaste React</h1>);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
