adde18b643c0cd871928717b086d3f45df52fbb5
Koushik <Koushikbajpayee06@gmail.com>
2025-09-19 19:55:35 +0530

    script added to run react app and nuild react app

--- App.js
+++ App.js
@@ -1,35 +1,8 @@
-/**
- *  <div id="parent">
- *      <div id='child'>
- *          <h1></h1>
- *      </div>
- *      <div id='child2'>
- *          <h2></h2>
- *      </div>
- *  </div>
- */
 import React from "react";
 import ReactDOM from "react-dom/client"
 
- const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    [React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"Hi I am h1 Tag"),
-        React.createElement("h2",{},"Hi I am h2 Tag")]
-    ),
-    React.createElement("div",{id:"child2"},
-        [React.createElement("h1",{},"Hi I am h1 Tag"),
-        React.createElement("h2",{},"Hi I am h2 Tag")]
-    ),
-    ]
-);
- 
+// React.createElement ==> Object
 
-
-
-
-// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!" )
-// console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent);
+const heading = React.createElement("h1",{id:"heading"},"Namaste Heading");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
