2a0031d5a8ed08053213371150e8a555f8906185
Koushik <Koushikbajpayee06@gmail.com>
2025-09-19 20:12:50 +0530

    this is how you create tage inside jsx

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client"
 
 // React.createElement ==> Object
 
-const heading = React.createElement("h1",{id:"heading"},"Namaste Heading");
+const heading = <h1 id="heading">Namaste React Using JSX</h1>
+
+// const heading = React.createElement("h1",{id:"heading"},"Namaste Heading");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
