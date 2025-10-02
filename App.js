1277c681596ccbe9ec917ee8415af2dc4b4c7e39
Koushik <Koushikbajpayee06@gmail.com>
2025-10-02 15:55:46 +0530

    Third Video End

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
+const elem = <span>React Element</span>
+
 const Title= () => (
     <h1 className="head">
+        {elem}
         Namaste React Using JSX
     </h1>
 )
 
 const HeadingComponent2 = ()=> (
     <div id="container">
-        <Title/>
+        {Title()}
+        {/* <Title/> */}
         <h1 className="heading">Namaste React</h1>    
     </div>
   
 );  
- 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<HeadingComponent2/>);
