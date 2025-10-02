c154e25ef0057b9bc8a9d43f4eedd9f3631c0176
Koushik <Koushikbajpayee06@gmail.com>
2025-10-02 17:30:56 +0530

    create card with static data

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
+const styleCard={
+    backgroundColor:"#f0f0f0"
+}
 const Header = () =>{
     return (
         <div className="header">
@@ -18,10 +21,74 @@ const Header = () =>{
         </div>
     )
 }
+
+const RestaurantCard = ()=>{
+    return(
+        <div className="res-card" style={styleCard}>
+            <img 
+            className="res-logo"
+             alt="res-logo"
+            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/u3cvjhtaopd4akomga4v"/>
+            <h3>Koshe Kosha</h3>
+            <h4>Bengali,North Indian</h4>
+            <h4>4.7 stars</h4>
+            <h4>38 minutes</h4>
+            
+        </div>
+    )
+}
+const Body = () =>{
+    return(
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+
+            </div>
+        </div>
+    )
+}
 const AppLayout = () =>{
     return (
         <div className="app">
             <Header/>
+            <Body/>
         </div>
     )
 }
