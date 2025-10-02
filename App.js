1201a09d99979817d9161676f13fe7c5a9d5ebc3
Koushik <Koushikbajpayee06@gmail.com>
2025-10-03 01:48:52 +0530

    optimized the code

--- App.js
+++ App.js
@@ -920,14 +920,9 @@ const Body = () =>{
                 Search
             </div>
             <div className="res-container">
-                <RestaurantCard resData={resList[0]}/>         
-                <RestaurantCard resData={resList[1]}/>         
-                <RestaurantCard resData={resList[2]}/>         
-                <RestaurantCard resData={resList[3]}/>         
-                <RestaurantCard resData={resList[4]}/>         
-                <RestaurantCard resData={resList[5]}/>         
-                <RestaurantCard resData={resList[6]}/>         
-                <RestaurantCard resData={resList[7]}/>         
+               { resList.map((restaurant, index) =>(
+                    <RestaurantCard key={index} resData={restaurant}/>
+                ))}
 
             </div>
         </div>
