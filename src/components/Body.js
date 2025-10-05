31e2f6c329006ceebc8e9de1e710203efeb15a24
Koushik <Koushikbajpayee06@gmail.com>
2025-10-05 23:08:11 +0530

    rerendering components with state variables

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,12 @@
+import {  useState } from "react";
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
+
 const Body = () =>{
-    let listOfRestaurants = [
-        {
-            "info": {
+  const [listOfRestaurants, setListOfRestaurants] = useState([
+      {
+        "info": {
                       "id": "769456",
                       "name": "Pizza Hut",
                       "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/ef0aff43-dcd4-4dca-9140-0aac4f52fe83_769456.JPG",
@@ -16,7 +18,7 @@ const Body = () =>{
                       "sla": {
                         "deliveryTime": 40,
                       },
-            },
+          },
         },
         {
             "info": {
@@ -32,17 +34,51 @@ const Body = () =>{
                         "deliveryTime": 24,
                       },
             },
-        },
+        }
+  ]);
+    // let listOfRestaurants = [
+    //     {
+    //         "info": {
+    //                   "id": "769456",
+    //                   "name": "Pizza Hut",
+    //                   "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/ef0aff43-dcd4-4dca-9140-0aac4f52fe83_769456.JPG",
+    //                   "costForTwo": "₹350 for two",
+    //                   "cuisines": [
+    //                     "Pizzas"
+    //                   ],
+    //                   "avgRating": 3.7,
+    //                   "sla": {
+    //                     "deliveryTime": 40,
+    //                   },
+    //         },
+    //     },
+    //     {
+    //         "info": {
+    //                   "id": "769457",
+    //                   "name": "Domminos",
+    //                   "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/ef0aff43-dcd4-4dca-9140-0aac4f52fe83_769456.JPG",
+    //                   "costForTwo": "₹450 for two",
+    //                   "cuisines": [
+    //                     "Pizzas"
+    //                   ],
+    //                   "avgRating": 4.5,
+    //                   "sla": {
+    //                     "deliveryTime": 24,
+    //                   },
+    //         },
+    //     },
 
-    ];
+    // ];
     return(
         <div className="body">
             <div className="filter">
                 <button className="filter-btn"
                  onClick={()=>{
                     //Filter Logic Here
-                    listOfRestaurants = listOfRestaurants.filter((x)=>x.info.avgRating>4);
-                    console.log(listOfRestaurants)
+                    const filteredList = listOfRestaurants.filter(
+                      (x)=>x.info.avgRating>4
+                    );
+                    setListOfRestaurants(filteredList);
                  }}>Top Rated Restaurants</button>
             </div>
             <div className="res-container">
