bb29a90e69e981275bf21c765323d73ea64f8a28
Koushik <Koushikbajpayee06@gmail.com>
2025-10-05 23:49:00 +0530

    passing entire data

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,38 +4,7 @@ import RestaurantCard from "./RestaurantCard";
 
 
 const Body = () =>{
-  const [listOfRestaurants, setListOfRestaurants] = useState([
-      {
-        "info": {
-                      "id": "769456",
-                      "name": "Pizza Hut",
-                      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/ef0aff43-dcd4-4dca-9140-0aac4f52fe83_769456.JPG",
-                      "costForTwo": "₹350 for two",
-                      "cuisines": [
-                        "Pizzas"
-                      ],
-                      "avgRating": 3.7,
-                      "sla": {
-                        "deliveryTime": 40,
-                      },
-          },
-        },
-        {
-            "info": {
-                      "id": "769457",
-                      "name": "Domminos",
-                      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/ef0aff43-dcd4-4dca-9140-0aac4f52fe83_769456.JPG",
-                      "costForTwo": "₹450 for two",
-                      "cuisines": [
-                        "Pizzas"
-                      ],
-                      "avgRating": 4.5,
-                      "sla": {
-                        "deliveryTime": 24,
-                      },
-            },
-        }
-  ]);
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
     // let listOfRestaurants = [
     //     {
     //         "info": {
@@ -82,8 +51,8 @@ const Body = () =>{
                  }}>Top Rated Restaurants</button>
             </div>
             <div className="res-container">
-               { listOfRestaurants.map((restaurant) =>(
-                    <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
+               { listOfRestaurants.map((restaurant,index) =>(
+                    <RestaurantCard key={restaurant.info.id } resData={restaurant}/>
                 ))}
             </div>
         </div>
