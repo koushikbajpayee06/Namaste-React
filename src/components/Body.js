0ebe5690977a9fdfc5fae014847fa44a40d630bf
Koushik <Koushikbajpayee06@gmail.com>
2025-10-09 23:56:09 +0530

    whole app got rid of mockData and Loading Live Data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,33 @@
 import {  useState , useEffect} from "react";
-import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 
 const Body = () =>{
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
   useEffect(()=>{
     console.log("useEffect called");
     fetchData();
   },[]);
 
-  const fetchData = async () =>{
-    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.56430&lng=88.36930&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
-    const json = await data.json();
-    console.log(json)
+const fetchData = async () => {
+  const data = await fetch(
+    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.56430&lng=88.36930&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+  );
+  const json = await data.json();
+  console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+);
 
-  }
+  // const restaurantList =
+  //   json?.data?.cards?.find(
+  //     (card) => card.card?.card?.id === "restaurant_grid_listing_v2"
+  //   )?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+
+  // console.log("🍽️ Restaurant List:", restaurantList);
+
+  setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+};
+
 
   console.log("Body called");
 
