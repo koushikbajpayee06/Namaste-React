5d5b01c4ef79b4cf3fb8b71c6488228beee57445
Koushik <Koushikbajpayee06@gmail.com>
2025-10-10 00:00:49 +0530

    fetch and rendered data

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,7 +15,7 @@ const fetchData = async () => {
     'https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.56430&lng=88.36930&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
   );
   const json = await data.json();
-  console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+  console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
 );
 
   // const restaurantList =
