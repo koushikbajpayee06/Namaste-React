ffbdd8452c19cd8de8e08086c00e105fdc9e3e06
Koushik <Koushikbajpayee06@gmail.com>
2025-10-10 10:17:41 +0530

    using ternary operator to write the condition

--- src/components/Body.js
+++ src/components/Body.js
@@ -29,45 +29,8 @@ const fetchData = async () => {
   setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
 };
 
-if(listOfRestaurants.length ===0){
-  return <ShimmerUI/>
-}
 
-
-    // let listOfRestaurants = [
-    //     {
-    //         "info": {
-    //                   "id": "769456",
-    //                   "name": "Pizza Hut",
-    //                   "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/ef0aff43-dcd4-4dca-9140-0aac4f52fe83_769456.JPG",
-    //                   "costForTwo": "₹350 for two",
-    //                   "cuisines": [
-    //                     "Pizzas"
-    //                   ],
-    //                   "avgRating": 3.7,
-    //                   "sla": {
-    //                     "deliveryTime": 40,
-    //                   },
-    //         },
-    //     },
-    //     {
-    //         "info": {
-    //                   "id": "769457",
-    //                   "name": "Domminos",
-    //                   "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/ef0aff43-dcd4-4dca-9140-0aac4f52fe83_769456.JPG",
-    //                   "costForTwo": "₹450 for two",
-    //                   "cuisines": [
-    //                     "Pizzas"
-    //                   ],
-    //                   "avgRating": 4.5,
-    //                   "sla": {
-    //                     "deliveryTime": 24,
-    //                   },
-    //         },
-    //     },
-
-    // ];
-    return(
+    return listOfRestaurants.length ===0 ? <ShimmerUI/>: (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn"
