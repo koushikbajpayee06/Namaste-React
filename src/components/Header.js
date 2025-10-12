7f81d20e5b9a8dd40d8792a9a6119c930e6772c2
Koushik <Koushikbajpayee06@gmail.com>
2025-10-12 23:59:26 +0530

    link to create routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 
 const Header = () =>{
@@ -18,8 +19,8 @@ const Header = () =>{
             <div className="nav-items">
                 <ul>
                     <li>Home</li>
-                    <li>About Us</li>
-                    <li>Conact Us</li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/contact">Conact Us</Link></li>
                     <li>Cart</li>
                     <button className="login-btn"
                      onClick={()=>{
