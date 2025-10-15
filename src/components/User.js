f2a04d746e901a387b988bbed56d3c25445ed193
Koushik <Koushikbajpayee06@gmail.com>
2025-10-15 15:43:39 +0530

    adding state variable in class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 
+useState
 const User = ({name,location})=>{
-
+    const [count] = useState(0);
     return (
         <div className="user-card">
+            <h1>Count:{count}</h1>
             <h2>{name}</h2>
             <h3>{location}</h3>
             <h3>Contact:@koushik6</h3>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,13 +4,19 @@ import React from "react";
 class UserClass extends React.Component{
         constructor(props){
         super(props);
-        console.log(props);
+        this.state = {
+            count:0,
+            count2:1
+        };
+        // console.log(props);
     }
     render(){
         const {name,location} = this.props;
 
             return (
                 <div className="user-card">
+                    <h1>Count:{this.state.count}</h1>
+                    <h1>Count2:{this.state.count2}</h1>
                     <h2>{name}</h2>
                     <h3>{location}</h3>
                     <h3>Contact:@koushik6</h3>
