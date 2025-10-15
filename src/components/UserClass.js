e6c670d0cdb31ca5a918ab7f05f16fffd7f033a0
Koushik <Koushikbajpayee06@gmail.com>
2025-10-15 12:21:09 +0530

    Create UserClass Class component same as User function Component

---
+++ src/components/UserClass.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+class UserClass extends React.Component{
+    render(){
+            return (
+                <div className="user-card">
+                    <h2>Name:Koushik</h2>
+                    <h3>Location:Kolkata</h3>
+                    <h3>Contact:@koushik6</h3>
+                </div>
+            )
+    }
+}
+export default UserClass
