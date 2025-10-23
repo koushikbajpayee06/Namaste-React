bf033a50d136ea6c103134ef1fea8e0da92c3b27
Koushik <Koushikbajpayee06@gmail.com>
2025-10-23 21:08:33 +0530

    pareent and child class component

--- src/components/About.js
+++ src/components/About.js
@@ -6,13 +6,13 @@ class About extends React.Component{
     constructor(props){
         super(props);
 
-        console.log("Parent Constructor")
+        //console.log("Parent Constructor")
     }
     componentDidMount(){
-        console.log("Parent component did mount")
+       // console.log("Parent component did mount")
     }
     render(){
-        console.log("Parent Render")
+        //console.log("Parent Render")
         return (
             <div>
                 <h1>About</h1>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,34 +5,24 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            count:0,
-            count2:1
         };
-        console.log("Child Constructor");
+        //console.log("Child Constructor");
     }
 
     componentDidMount(){
-        console.log("child component did mount")
+        //console.log("child component did mount");
+        // API Call
+
     }
 
     render(){
         const {name,location} = this.props;
-        const {count} = this.state;
 
-        console.log("Child Render");
+
+        //console.log("Child Render");
             return (
                 <div className="user-card">
-                    <h1>Count:{count}</h1>
-                    <button
-                     onClick={()=>{
-                        //NEVER UPDATE STATE VARIABLE DITECTLY
-                        this.setState(
-                        {
-                            count: this.state.count+1,
-                            count2:this.state.count2+1,
-                        });
-     
-                    }}>Increase Count</button>
+                   
                     <h2>{name}</h2>
                     <h3>{location}</h3>
                     <h3>Contact:@koushik6</h3>
